8087e1cb9cce6830aae1992a2b3cbe5f10145c2b
andrewbraunsdorf <45677101+andrewbraunsdorf@users.noreply.github.com>
2018-12-24 13:02:49 -0500

    Create objects02.js

---
+++ objects02.js
@@ -0,0 +1,8 @@
+var pizza = {
+     company: "Pizza Zilla",
+     section: "Northeast",
+     locations: 25,
+     hoursOpen: 12,
+};
+
+console.log(pizza.company, "located in", pizza.section, "has a total open hours of", pizza.locations * pizza.hoursOpen + ".");
