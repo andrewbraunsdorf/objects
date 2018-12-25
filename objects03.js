6367d27dabed7a9c53716d7ac6e8dd510982721d
andrewbraunsdorf <45677101+andrewbraunsdorf@users.noreply.github.com>
2018-12-25 15:22:23 -0500

    Create objects03.js

---
+++ objects03.js
@@ -0,0 +1,7 @@
+var pasta = {
+     kind: "Italian",
+     type: "Rotini",
+     lbs: 1338,
+     pricePerLB: 0.13
+};
+console.log(pasta.kind, "total cost of", pasta.type, "is", pasta.lbs * pasta.pricePerLB + ".");
